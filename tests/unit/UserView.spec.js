0142f83db82b58ec6ddbe93a7ea51d316ca06245
NeilWkz <neilross.wkzdesign@gmail.com>
2019-03-23 21:39:44 +0000

    refactor test to keep it DRY

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -1,25 +1,31 @@
 import { shallowMount } from "@vue/test-utils";
 import UserView from "@/views/UserView";
-import VRatingRangeCtrl from '@/components/VRatingRangeCtrl';
-import VMovieList from '@/components/VMovieList';
-
+import VRatingRangeCtrl from "@/components/VRatingRangeCtrl";
+import VMovieList from "@/components/VMovieList";
 describe("UserView", () => {
-  it("renders the component", () => {
-    // arrange
+  const build = () => {
     const wrapper = shallowMount(UserView);
 
+    return {
+      wrapper,
+      userMovieList: () => wrapper.find(VMovieList),
+      userRatingRangeCtrl: () => wrapper.find(VRatingRangeCtrl)
+    };
+  };
+
+  it("renders the component", () => {
+    // arrange
+    const { wrapper } = build();
     // assert
     expect(wrapper.html()).toMatchSnapshot();
   });
 
-  it("renders the child components", () => {
+  it("renders main child components", () => {
     // arrange
-    const wrapper = shallowMount(UserView);
-    const userMovieList = wrapper.find(VMovieList);
-    const userRatingRangeCtrl = wrapper.find(VRatingRangeCtrl);
+    const { userMovieList, userRatingRangeCtrl } = build();
 
     // assert
-    expect(userMovieList.exists()).toBe(true);
-    expect(userRatingRangeCtrl.exists()).toBe(true);
+    expect(userMovieList().exists()).toBe(true);
+    expect(userRatingRangeCtrl().exists()).toBe(true);
   });
 });
