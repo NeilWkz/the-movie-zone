73924716b273e3eb71b73feb2c420d061c02c10b
NeilWkz <neilross.wkzdesign@gmail.com>
2019-03-23 21:44:22 +0000

    Test the props passing to the range ctrl

--- tests/unit/UserView.spec.js
+++ tests/unit/UserView.spec.js
@@ -2,6 +2,7 @@ import { shallowMount } from "@vue/test-utils";
 import UserView from "@/views/UserView";
 import VRatingRangeCtrl from "@/components/VRatingRangeCtrl";
 import VMovieList from "@/components/VMovieList";
+
 describe("UserView", () => {
   const build = () => {
     const wrapper = shallowMount(UserView);
@@ -27,5 +28,17 @@ describe("UserView", () => {
     // assert
     expect(userMovieList().exists()).toBe(true);
     expect(userRatingRangeCtrl().exists()).toBe(true);
+  });
+
+  it("Passes a binded prop to the rating range component", () => {
+    //arrange
+    const { wrapper, userRatingRangeCtrl } = build();
+    wrapper.setData({
+      rating: {
+        initial: 3
+      }
+    });
+    //assert
+    expect(userRatingRangeCtrl().vm.rating).toBe(wrapper.vm.rating);
   });
 });
