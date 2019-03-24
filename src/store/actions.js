5ab79550b0623556cf70c140c6ed972c8900f65d
NeilWkz <neilross.wkzdesign@gmail.com>
2019-03-24 17:22:23 +0000

    tranform movie object to have genre names

--- src/store/actions.js
+++ src/store/actions.js
@@ -5,7 +5,7 @@ export default {
     return new Promise(async (resolve, reject) => {
       try {
         const movies = await api.getMovies();
-        commit("SET_MOVIES", movies);
+        // commit("SET_MOVIES", movies);
         return resolve(movies);
       } catch (error) {
         reject(error);
@@ -25,15 +25,12 @@ export default {
   },
 
   async GET_ALL_DATA({ dispatch, commit }) {
+    // GET_ALL_DATA triggers the actions that get the data from the API and then proccesses it before commiting to state
 
-  // GET_ALL_DATA triggers the actions that get the data from the API and then proccesses it before commiting to state
-    
     // Call and then wait for `GET_MOVIES` & GET_GENRES to finish
     let movies = await dispatch("GET_MOVIES");
     let genres = await dispatch("GET_GENRES");
-    // console.log(movies);
-    //  console.log(genres);
-
+    
     let movieGenreIds = [];
     let deduped;
 
@@ -55,5 +52,27 @@ export default {
       return Array.from(filtered);
     };
     commit("SET_GENRES", filterGenres(genres));
+
+    let genreTitles = [];
+    let movieGenres = [];
+
+    function valuesToArray(obj) {
+      return Object.keys(obj).map(function(key) {
+        return obj[key];
+      });
+    }
+    let moviesWithGenres = (movies, genres) => {
+      //loop through the all the results
+      movies.forEach(movie => {
+        //match the genre titles to the movie
+        genreTitles = genres.filter(el => movie.genre_ids.includes(el.id));
+        movie.genre_titles = valuesToArray(genreTitles);
+        movieGenres.push(movie);
+      });
+
+      return movieGenres;
+    };
+    commit("SET_MOVIES", moviesWithGenres(movies, genres));
+    
   }
 };
