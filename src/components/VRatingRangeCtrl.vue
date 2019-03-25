41320ce7059645f2a2283cea856ec565b2bea0fe
NeilWkz <neilross.wkzdesign@gmail.com>
2019-03-25 23:38:47 +0000

    refactoring actions and api tests

---
+++ src/components/VRatingRangeCtrl.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+  name: "RatingRangeCtrl",
+
+  props: {
+    rating: {
+      type: Object,
+      required: true,
+      default: () => ({})
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+   
+    
+  </div>
+</template>
