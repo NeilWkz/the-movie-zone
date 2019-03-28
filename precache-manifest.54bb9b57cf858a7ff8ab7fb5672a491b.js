726566c10da1c7be36f343d3baa4fa672fbbef3b
NeilWkz <neilross.wkzdesign@gmail.com>
2019-03-28 11:41:36 +0000

    deploy

---
+++ precache-manifest.54bb9b57cf858a7ff8ab7fb5672a491b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/the-movie-zone/robots.txt"
+  },
+  {
+    "revision": "1adf443b6cf3f06caf5b",
+    "url": "/the-movie-zone/js/chunk-vendors.9c670f0d.js"
+  },
+  {
+    "revision": "b43e132bafb7e303d31f",
+    "url": "/the-movie-zone/js/app.ec88e43d.js"
+  },
+  {
+    "revision": "42d0c51a6826cb164ca794973f0f01e9",
+    "url": "/the-movie-zone/index.html"
+  },
+  {
+    "revision": "877893123523865fc326df0bafd44e45",
+    "url": "/the-movie-zone/img/the-movie-zone-logo.svg"
+  },
+  {
+    "revision": "c4cddfe473b5e7d7247c115f04ad1f3b",
+    "url": "/the-movie-zone/img/movie-zone-logo.svg"
+  },
+  {
+    "revision": "fec70503a093775204bc11ba362569ae",
+    "url": "/the-movie-zone/img/cinema-hero.jpg"
+  },
+  {
+    "revision": "1adf443b6cf3f06caf5b",
+    "url": "/the-movie-zone/css/chunk-vendors.b837c4aa.css"
+  },
+  {
+    "revision": "b43e132bafb7e303d31f",
+    "url": "/the-movie-zone/css/app.b29c1bdc.css"
+  }
+];
